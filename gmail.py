af5f236fc4cba8b872f5f010b38d943318ba73db
BakaYaroslav <jaroslavbaka25@gmail.com>
2026-01-08 14:12:47 +0200

    .

--- gmail.py
+++ gmail.py
@@ -21,12 +21,16 @@ message = EmailMessage()
 
 with open('message.html', 'r', encoding='utf-8') as file:
  file_content = file.read()
+message.set_content(file_content, subtype='html')
+
+with open('Python-logo-notext.svg.png', 'rb') as file:
+ image_data = file.read()
+message.add_attachment(image_data, maintype='image', subtype='png', filename='Python-logo-notext.svg.png')
 
 message['Subject'] = email_subject
 message['From'] = sender_email_address
 message['To'] = receiver_email_address
 
-message.add_alternative(file_content, subtype='html')
 
 server = smtplib.SMTP(email_smtp, '587')
 
